56d66f992f06e3a5dab96bf4f62dd2548d904e52
ghostofgamer <vadimcapital@mail.ru>
2024-12-27 19:15:46 +0400

    AddDate

--- Assets/Scripts/UI/Screens/HomeScreen.cs
+++ Assets/Scripts/UI/Screens/HomeScreen.cs
@@ -1,6 +1,22 @@
+using System;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 
 public class HomeScreen : MonoBehaviour
 {
-
+    [SerializeField] private TMP_Text _date;
+    
+    private void OnEnable()
+    {
+        UpdateDateText();
+    }
+    
+    private void UpdateDateText()
+    {
+        DateTime currentDate = DateTime.Now;
+        CultureInfo culture = new CultureInfo("en-US");
+        string formattedDate = currentDate.ToString("dddd, d MMMM", culture);
+        _date.text = formattedDate;
+    }
 }
